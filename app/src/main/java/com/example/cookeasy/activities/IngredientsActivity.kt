1570e4f1ec77ea464eaf0396c98d7e0f32936092
Juila Li <lijulia@wustl.edu>
2020-04-06 20:21:50 -0500

    ingredient list can write data

--- app/src/main/java/com/example/cookeasy/activities/IngredientsActivity.kt
+++ app/src/main/java/com/example/cookeasy/activities/IngredientsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.cookeasy.activities
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.Sampler
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookeasy.R
@@ -16,6 +19,8 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_ingredients.*
+import kotlinx.android.synthetic.main.enter_ingredient.*
+import kotlinx.android.synthetic.main.enter_ingredient.view.*
 
 class IngredientsActivity : AppCompatActivity() {
 
@@ -23,11 +28,12 @@ class IngredientsActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var database: DatabaseReference
 
-    private var leaderList = ArrayList<User>()
 
     private lateinit var rView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private var ingredientList = ArrayList<IngredientItem>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +50,6 @@ class IngredientsActivity : AppCompatActivity() {
 
 
     private fun generateIngredientList(size: Int) {
-        val list = ArrayList<IngredientItem>()
         val nameList = ArrayList<String>()
 
         val uid = FirebaseAuth.getInstance().currentUser!!.uid
@@ -57,13 +62,16 @@ class IngredientsActivity : AppCompatActivity() {
 
                 p0.children.forEach {
                     Log.d("in foreach", "hi")
-                    Log.d("ingredient", it.child("ingredient").getValue().toString())
-                    val ingredient = it.child("ingredient").getValue()
-                    val item = IngredientItem(ingredient.toString())
-                    list.add(item)
+//                    Log.d("ingredient", it.child("ingredient").getValue().toString())
+                    Log.d("ingredient", it.child("name").getValue(String::class.java).toString())
+                    val ingredient = it.child("name").getValue(String::class.java).toString()
+                    if(ingredient != null) {
+                        val item = IngredientItem(ingredient.toString())
+                        ingredientList.add(item)
+                    }
                 }
 
-                recyclerView.adapter = IngredientsAdapter(list)
+                recyclerView.adapter = IngredientsAdapter(ingredientList)
                 recyclerView.layoutManager = LinearLayoutManager(this@IngredientsActivity)
                 recyclerView.setHasFixedSize(true)
             }
@@ -74,6 +82,34 @@ class IngredientsActivity : AppCompatActivity() {
         })
 
 //        return list
+    }
+
+    fun dialogView(view: View) {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.enter_ingredient, null)
+        val builder = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setTitle("Enter the ingredient")
+        val alertDialog = builder.show()
+
+        alertDialog.submitItem.setOnClickListener {
+            val ingredientInput = dialogView.name.text.toString()
+            if(ingredientInput != ""){
+                writeNewIngredient(ingredientInput)
+                val item = IngredientItem(ingredientInput)
+                ingredientList.add(item)
+                recyclerView.adapter = IngredientsAdapter(ingredientList)
+                recyclerView.layoutManager = LinearLayoutManager(this@IngredientsActivity)
+                recyclerView.setHasFixedSize(true)
+                alertDialog.dismiss()
+            }
+        }
+    }
+
+    private fun writeNewIngredient(ingredientName: String) {
+//        val uid = FirebaseAuth.getInstance().uid?: ""
+        val ingredient = IngredientItem(ingredientName)
+//        FirebaseDatabase.getInstance().getReference("/users/$uid").setValue(user)
+        FirebaseDatabase.getInstance().getReference("/ingredients").push().setValue(ingredient)
     }
 
 }
